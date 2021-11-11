60606926fbae369c23d6a2d390de51d9c5175764
uri <794866@unizar.es>
2021-11-11 23:17:38 +0100

    makin files struct

---
+++ simuladores/distconssim/packages/lefs_test.go
@@ -0,0 +1,16 @@
+package packages
+
+import (
+	"log"
+	"testing"
+)
+
+func TestLefsLoad(t *testing.T) {
+	log.Printf("************************** Basic lefs loading test ....")
+	lefs, err := Load("testdata/twoTwo.json")
+	if err != nil {
+		println("Couln't load th pn file !")
+	}
+
+	lefs.ImprimeLefs()
+}
